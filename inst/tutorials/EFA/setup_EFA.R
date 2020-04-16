6cb1b08760d511cd491363ed06b443f7e68cc60e
jpirmer <julienirmer_17@yahoo.de>
2020-04-16 16:19:39 +0200

    Add Sitzung_2 trial run.

---
+++ inst/tutorials/EFA/setup_EFA.R
@@ -0,0 +1,12 @@
+#### Setup for intro in learnr ####
+
+### Require packages ----
+library(learnr)       # package to generate interactive HTML
+library(gradethis)    # grade Code from input in HTML
+library(shiny)        # nice appearance in R
+library(fontawesome)  # nice fonts
+library(corrplot)     # Korrelationsmatrix grafisch darstellen
+library(psych)        # EFA durchführen
+library(GPArotation)  # EFA Lösung rotieren
+
+data(Big5_short, package = 'PsyMSc1')
--- inst/tutorials/EFA/setup_backup.R
+++
@@ -1,9 +0,0 @@
-#### Setup for intro in learnr ####
-
-### Require packages ----
-library(learnr)
-library(shiny)
-library(fontawesome)
-library(lavaan)
-
-data(fairplayer, package = 'PsyMSc1')
