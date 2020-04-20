102fc65aacbd0174b75f67343a6237a25d9193fc
jpirmer <julienirmer_17@yahoo.de>
2020-04-20 16:55:12 +0200

    Add data description for Sitzung2.

--- R/data.R
+++ R/data.R
@@ -1,3 +1,12 @@
+#' Big5 Daten (2014)
+#' 
+#' Selbstberichte zu Persönlichkeitseigenschaften. Gekürzter Datensatz, der nur jeweils die ersten 3 Items zu den fünf Persönlichkeitseigenschaften "Extraversion", "Neurotizismus", "Offenheit", "Verträglichkeit" und "Gewissenhaftigkeit".
+#' 
+#'  @format Zu den demografischen Daten gehören Alter (`age`), Englisch als Muttersprache (`engant`, 1=yes, 2=no, 0=missed), Geschlecht (`gender`, 1=Male, 2=Female, 3=Other, 0=missed) und Herkunftsland (`country`, ISO-kodiert, bspw. "DE" = Deutschland, "FR" = Frankreich, "EM" = Vereinigte Arabische Emirate, "US" = Vereinigten Staaten von Amerika). Die Persönlichkeitseigeschaften sind: Extraversion (engl. *extraversion*, Items: `E1`, `E2`, `E3`), Neurotizismus (engl. *neuroticism*, Items: `N1`, `N2`, `N3`),  Verträglichkeit (engl. *agreeableness*, Items: `A1`, `A2`, `A3`),  Gewissenhaftigkeit  (engl. *conscientiousness*, Items: `C1`, `C2`, `C3`) und  Offenheit für Erfahrungen (engl. *openness*, Items: `O1`, `O2`, `O3`).
+#' 
+#' @source Der Originaldatensatz ist ein Onlinedatensatz, wird seit 2012 erfasst und ist auf [openpsychometrics.org](https://openpsychometrics.org/_rawdata/) als *.zip* downloadbar.
+"Big5"
+
 #' fairplayer Daten (2009)
 #' 
 #' Selbstberichte zur relationalen Aggression, Empathie und sozialen Intelligenz aus einer Studie zur Prävention von Bullying bei Jugendlichen.
@@ -8,11 +17,3 @@
 #' 
 "fairplayer"
 
-#' Big5 Daten
-#' 
-#' Selbstberichte zu Persönlichkeitseigenschaften. Gekürzter Datensatz, der nur jeweils die ersten 3 Items zu den fünf Persönlichkeitseigenschaften "Extraversion", "Neurotizismus", "Offenheit", "Verträglichkeit" und "Gewissenhaftigkeit".
-#' 
-#'  @format Zu den demografischen Daten gehören Alter (`age`), Englisch als Muttersprache (`engant`, 1=yes, 2=no, 0=missed), Geschlecht (`gender`, 1=Male, 2=Female, 3=Other, 0=missed) und Herkunftsland (`country`, ISO-kodiert, bspw. "DE" = Deutschland, "FR" = Frankreich, "EM" = Vereinigte Arabische Emirate, "US" = Vereinigten Staaten von Amerika). Die Persönlichkeitseigeschaften sind: Extraversion (engl. *extraversion*, Items: `E1`, `E2`, `E3`), Neurotizismus (engl. *neuroticism*, Items: `N1`, `N2`, `N3`),  Verträglichkeit (engl. *agreeableness*, Items: `A1`, `A2`, `A3`),  Gewissenhaftigkeit  (engl. *conscientiousness*, Items: `C1`, `C2`, `C3`) und  Offenheit für Erfahrungen (engl. *openness*, Items: `O1`, `O2`, `O3`).
-#' 
-#' @source Der Originaldatensatz ist ein Onlinedatensatz, wird seit 2012 erfasst und ist auf [openpsychometrics.org](https://openpsychometrics.org/_rawdata/) als *.zip* downloadbar.
-"Big5"
