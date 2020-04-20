5c1d76947fcf371980b97795e3c9170521c42792
jpirmer <julienirmer_17@yahoo.de>
2020-04-20 16:51:11 +0200

    Add data description for Sitzung2.

--- R/data.R
+++ R/data.R
@@ -15,5 +15,4 @@
 #'  @format Zu den demografischen Daten gehören Alter (`age`), Englisch als Muttersprache (`engant`, 1=yes, 2=no, 0=missed), Geschlecht (`gender`, 1=Male, 2=Female, 3=Other, 0=missed) und Herkunftsland (`country`, ISO-kodiert, bspw. "DE" = Deutschland, "FR" = Frankreich, "EM" = Vereinigte Arabische Emirate, "US" = Vereinigten Staaten von Amerika). Die Persönlichkeitseigeschaften sind: Extraversion (engl. *extraversion*, Items: `E1`, `E2`, `E3`), Neurotizismus (engl. *neuroticism*, Items: `N1`, `N2`, `N3`),  Verträglichkeit (engl. *agreeableness*, Items: `A1`, `A2`, `A3`),  Gewissenhaftigkeit  (engl. *conscientiousness*, Items: `C1`, `C2`, `C3`) und  Offenheit für Erfahrungen (engl. *openness*, Items: `O1`, `O2`, `O3`).
 #' 
 #' @source Der Originaldatensatz ist ein Onlinedatensatz, wird seit 2012 erfasst und ist auf [openpsychometrics.org](https://openpsychometrics.org/_rawdata/) als *.zip* downloadbar.
-#' 
 "Big5"
