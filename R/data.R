acb35201d048228dddee73160886b0e15e5ddf15
jpirmer <julienirmer_17@yahoo.de>
2020-04-20 15:06:14 +0200

    Add data description for Sitzung2.

--- R/data.R
+++ R/data.R
@@ -7,3 +7,13 @@
 #' @source Bull, H., Schultze, M., Scheithauer, H. (2009) School-based prevention of bullying and relational aggression: The fairplayer.manual. European Journal of Developmental Science, 3:313-317.
 #' 
 "fairplayer"
+
+#' Big5 Daten
+#' 
+#' Selbstberichte zu Persönlichkeitseigenschaften. Gekürzter Datensatz, der nur jeweils die ersten 3 Items zu den fünf Persönlichkeitseigenschaften "Extraversion", "Neurotizismus", "Offenheit", "Verträglichkeit" und "Gewissenhaftigkeit".
+#' 
+#'  @format Zu den demografischen Daten gehören Alter (`age`), Englisch als Muttersprache (`engant`, 1=yes, 2=no, 0=missed), Geschlecht (`gender`, 1=Male, 2=Female, 3=Other, 0=missed) und Herkunftsland (`country`, ISO-kodiert, bspw. "DE" = Deutschland, "FR" = Frankreich, "EM" = Vereinigte Arabische Emirate, "US" = Vereinigten Staaten von Amerika). Die Persönlichkeitseigeschaften sind: Extraversion (engl. *extraversion*, Items: `E1`, `E2`, `E3`), Neurotizismus (engl. *neuroticism*, Items: `N1`, `N2`, `N3`),  Verträglichkeit (engl. *agreeableness*, Items: `A1`, `A2`, `A3`),  Gewissenhaftigkeit  (engl. *conscientiousness*, Items: `C1`, `C2`, `C3`) und  Offenheit für Erfahrungen (engl. *openness*, Items: `O1`, `O2`, `O3`).
+#' 
+#' @source Der Originaldatensatz ist ein Onlinedatensatz, wird seit 2012 erfasst und ist auf [openpsychometrics.org](https://openpsychometrics.org/_rawdata/) als *.zip* downloadbar.
+#' 
+"Big5"
