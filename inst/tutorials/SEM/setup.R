c344a1d7a2703993006d5a6dfc4eb0bc9feb0fd6
jpirmer <julienirmer_17@yahoo.de>
2020-05-31 13:18:44 +0200

    Initial commit Sitzung4.

---
+++ inst/tutorials/SEM/setup.R
@@ -0,0 +1,10 @@
+#### Setup for intro in learnr ####
+
+### Require packages ----
+library(learnr)       # package to generate interactive HTML
+library(gradethis)    # grade Code from input in HTML
+library(shiny)        # nice appearance in R
+library(fontawesome)  # nice fonts
+library(corrplot)     # Korrelationsmatrix grafisch darstellen
+library(psych)        # EFA durchführen
+
