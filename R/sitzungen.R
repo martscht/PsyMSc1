83d99d20b85b85a80b6e2225bc385de96d69b2b5
jpirmer <julienirmer_17@yahoo.de>
2020-06-10 17:35:07 +0200

    Add function call of Sitzung_4().

--- R/sitzungen.R
+++ R/sitzungen.R
@@ -67,5 +67,5 @@ Sitzung_4 <- function() {
     devtools::install_github('rstudio-education/gradethis', upgrade = 'never')
   }
   library(gradethis)
-  message('Leider ist die Sitzung noch nicht fertig :(.')
+  compile('SEM')
 }
